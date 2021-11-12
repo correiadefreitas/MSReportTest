7ca39bf19175a249eea2d19d105403960ce319fc
Correia de Freitas <correiadefreitas@gmail.com>
2021-11-12 15:46:49 +0000

    More stuff

--- ReportsTest/Form1.Designer.vb
+++ ReportsTest/Form1.Designer.vb
@@ -29,6 +29,7 @@ Partial Class Form1
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         CType(Me.VendasDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -84,11 +85,24 @@ Partial Class Form1
         Me.Button1.Text = "Ver"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(97, 34)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(80, 17)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "Ver detalhe"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(943, 585)
+        Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ComboBox1)
@@ -108,4 +122,5 @@ Partial Class Form1
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
 End Class
--- ReportsTest/Form1.vb
+++ ReportsTest/Form1.vb
@@ -1,10 +1,31 @@
 ﻿Imports Microsoft.Reporting.WinForms
 
 Public Class Form1
+
+    Private _NumeroEncomenda As Integer = 1
+    Public Property NumeroEncomenda() As Integer
+        Get
+            Return _NumeroEncomenda
+        End Get
+        Set(ByVal value As Integer)
+            _NumeroEncomenda = value
+        End Set
+    End Property
+
+    Private _VerDetalhe As Boolean
+    Public Property VerDetalhe() As Boolean
+        Get
+            Return _VerDetalhe
+        End Get
+        Set(ByVal value As Boolean)
+            _VerDetalhe = value
+        End Set
+    End Property
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        Me.ORDER_DATATableAdapter1.Fill(Me.VendasDataSet1.ORDER_DATA)
-        Me.LINEITEMTableAdapter1.Fill(Me.VendasDataSet1.LINEITEM)
+        ORDER_DATATableAdapter1.Fill(Me.VendasDataSet1.ORDER_DATA)
+        'LINEITEMTableAdapter1.Fill(Me.VendasDataSet1.LINEITEM)
 
         AtualizarRelatorio()
     End Sub
@@ -14,14 +35,11 @@ Public Class Form1
         ReportViewer1.Visible = False
         ReportViewer1.LocalReport.DataSources.Clear()
 
-
-
-
         Console.WriteLine(DirectCast(ComboBox1.SelectedValue, Integer))
         Console.WriteLine(Me.ORDER_DATATableAdapter1.GetData().Rows.Count())
 
 
-        Dim dtOrderData As DataTable = Me.ORDER_DATATableAdapter1.GetData().Where(Function(x) x.ORDERID = DirectCast(ComboBox1.SelectedValue, Integer)).CopyToDataTable()
+        Dim dtOrderData As DataTable = Me.ORDER_DATATableAdapter1.GetData().Where(Function(x) x.ORDERID = NumeroEncomenda).CopyToDataTable()
         dtOrderData.TableName = "DadosEncomenda"
 
         Dim dtLineItem = Me.LINEITEMTableAdapter1.GetData().Where(Function(x) x.ORDERID = DirectCast(ComboBox1.SelectedValue, Integer)).CopyToDataTable()
@@ -31,12 +49,15 @@ Public Class Form1
         Dim rdsOrderData = New ReportDataSource("DadosEncomenda", dtOrderData)
         Dim rdsLineItem = New ReportDataSource("DetalheEncomenda", dtLineItem)
 
-        Me.ReportViewer1.LocalReport.DataSources.Add(rdsOrderData)
-        Me.ReportViewer1.LocalReport.DataSources.Add(rdsLineItem)
+        ReportViewer1.LocalReport.DataSources.Add(rdsOrderData)
+        ReportViewer1.LocalReport.DataSources.Add(rdsLineItem)
+
+        ReportViewer1.LocalReport.SetParameters(New ReportParameter("MostrarDetalhe", VerDetalhe))
 
         AddHandler ReportViewer1.LocalReport.SubreportProcessing, AddressOf ProcessarSubReport
 
         Me.ReportViewer1.RefreshReport()
+        Application.DoEvents()
         ReportViewer1.Visible = True
     End Sub
 
@@ -44,7 +65,7 @@ Public Class Form1
 
         e.DataSources.Clear()
 
-        Dim dt As DataTable = Me.LINEITEMTableAdapter1.GetData()
+        Dim dt As DataTable = LINEITEMTableAdapter1.GetData().Where(Function(x) x.ORDERID = NumeroEncomenda).CopyToDataTable()
         dt.TableName = "DetalheEncomenda"
         Dim rds = New ReportDataSource("DetalheEncomenda", dt)
 
@@ -53,5 +74,13 @@ Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         AtualizarRelatorio()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        NumeroEncomenda = DirectCast(sender, ComboBox).SelectedValue
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        VerDetalhe = DirectCast(sender, CheckBox).Checked
     End Sub
 End Class
