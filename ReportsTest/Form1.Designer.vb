0c4baf5c2f6f8df540e329d0ee359534795115d0
Correia de Freitas <correiadefreitas@gmail.com>
2021-11-12 09:47:04 +0000

    Report working

---
+++ ReportsTest/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.ORDER_DATATableAdapter1 = New ReportsTest.VendasDataSetTableAdapters.ORDER_DATATableAdapter()
+        Me.VendasDataSet1 = New ReportsTest.VendasDataSet()
+        Me.LINEITEMTableAdapter1 = New ReportsTest.VendasDataSetTableAdapters.LINEITEMTableAdapter()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.VendasDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.LocalReport.DisplayName = "Report de teste"
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "ReportsTest.ReportTeste.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(12, 193)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.ServerReport.BearerToken = Nothing
+        Me.ReportViewer1.Size = New System.Drawing.Size(919, 380)
+        Me.ReportViewer1.TabIndex = 0
+        '
+        'ORDER_DATATableAdapter1
+        '
+        Me.ORDER_DATATableAdapter1.ClearBeforeFill = True
+        '
+        'VendasDataSet1
+        '
+        Me.VendasDataSet1.DataSetName = "VendasDataSet"
+        Me.VendasDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'LINEITEMTableAdapter1
+        '
+        Me.LINEITEMTableAdapter1.ClearBeforeFill = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DataSource = Me.VendasDataSet1
+        Me.ComboBox1.DisplayMember = "ORDER_DATA.ORDERID"
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(97, 6)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 1
+        Me.ComboBox1.ValueMember = "ORDER_DATA.ORDERID"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Encomenda Nº"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(224, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Ver"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(943, 585)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.VendasDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents ORDER_DATATableAdapter1 As VendasDataSetTableAdapters.ORDER_DATATableAdapter
+    Friend WithEvents VendasDataSet1 As VendasDataSet
+    Friend WithEvents LINEITEMTableAdapter1 As VendasDataSetTableAdapters.LINEITEMTableAdapter
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ ReportsTest/Form1.vb
@@ -0,0 +1,57 @@
+﻿Imports Microsoft.Reporting.WinForms
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.ORDER_DATATableAdapter1.Fill(Me.VendasDataSet1.ORDER_DATA)
+        Me.LINEITEMTableAdapter1.Fill(Me.VendasDataSet1.LINEITEM)
+
+        AtualizarRelatorio()
+    End Sub
+
+    Private Sub AtualizarRelatorio()
+
+        ReportViewer1.Visible = False
+        ReportViewer1.LocalReport.DataSources.Clear()
+
+
+
+
+        Console.WriteLine(DirectCast(ComboBox1.SelectedValue, Integer))
+        Console.WriteLine(Me.ORDER_DATATableAdapter1.GetData().Rows.Count())
+
+
+        Dim dtOrderData As DataTable = Me.ORDER_DATATableAdapter1.GetData().Where(Function(x) x.ORDERID = DirectCast(ComboBox1.SelectedValue, Integer)).CopyToDataTable()
+        dtOrderData.TableName = "DadosEncomenda"
+
+        Dim dtLineItem = Me.LINEITEMTableAdapter1.GetData().Where(Function(x) x.ORDERID = DirectCast(ComboBox1.SelectedValue, Integer)).CopyToDataTable()
+        dtLineItem.TableName = "DetalheEncomenda"
+
+
+        Dim rdsOrderData = New ReportDataSource("DadosEncomenda", dtOrderData)
+        Dim rdsLineItem = New ReportDataSource("DetalheEncomenda", dtLineItem)
+
+        Me.ReportViewer1.LocalReport.DataSources.Add(rdsOrderData)
+        Me.ReportViewer1.LocalReport.DataSources.Add(rdsLineItem)
+
+        AddHandler ReportViewer1.LocalReport.SubreportProcessing, AddressOf ProcessarSubReport
+
+        Me.ReportViewer1.RefreshReport()
+        ReportViewer1.Visible = True
+    End Sub
+
+    Private Sub ProcessarSubReport(sender As Object, e As SubreportProcessingEventArgs)
+
+        e.DataSources.Clear()
+
+        Dim dt As DataTable = Me.LINEITEMTableAdapter1.GetData()
+        dt.TableName = "DetalheEncomenda"
+        Dim rds = New ReportDataSource("DetalheEncomenda", dt)
+
+        e.DataSources.Add(rds)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        AtualizarRelatorio()
+    End Sub
+End Class
