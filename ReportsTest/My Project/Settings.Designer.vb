21c3e906f95e1c07532e2417e0b3883f3eaeeb2d
Correia de Freitas <correiadefreitas@gmail.com>
2021-11-17 11:11:30 +0000

    Missing DB

--- ReportsTest/My Project/Settings.Designer.vb
+++ ReportsTest/My Project/Settings.Designer.vb
@@ -53,17 +53,7 @@ Namespace My
                 Return defaultInstance
             End Get
         End Property
-        
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=AZULBANANA;Initial Catalog=Vendas;Integrated Security=True")>  _
-        Public ReadOnly Property VendasConnectionString() As String
-            Get
-                Return CType(Me("VendasConnectionString"),String)
-            End Get
-        End Property
-        
+
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
